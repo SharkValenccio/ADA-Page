4039444b073f1b7171ce5e13fd3dd7e95d304dc6
SharkValenccio <sharkvalenccio@gmail.com>
2023-07-16 20:42:11 -0300

    Animation radius menu On

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ const nav = document.querySelector("nav"),
 toggleBtn.addEventListener("click" , () =>{
     nav.classList.toggle("open");
 });
+
+var iconMenu = document.getElementById("iconMenu")
+
+iconMenu.addEventListener("click", function() {
+
+ var menuRadius = document.getElementById("menuRadius");
+
+ if(menuRadius.style.display === "flex"){
+    menuRadius.style.display = "none";
+ } else {
+    menuRadius.style.display = "flex";
+ }
+}); 
